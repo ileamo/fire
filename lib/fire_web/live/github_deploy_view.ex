8fbc945dc204dc49184f3c36f879ba1807e5f97e
imosunov <imosunov@nsg.net.ru>
2019-06-16 00:02:05 +0300

    test

--- lib/fire_web/live/github_deploy_view.ex
+++ lib/fire_web/live/github_deploy_view.ex
@@ -5,7 +5,10 @@ defmodule FireWeb.GithubDeployView do
     ~L"""
     <div class="">
       <div>
-        <%= @deploy_step %>
+        <div>
+          <button phx-click="github_deploy">Deploy to GitHub</button>
+        </div>
+        Status: <%= @deploy_step %>
       </div>
     </div>
     """
@@ -14,4 +17,54 @@ defmodule FireWeb.GithubDeployView do
   def mount(_session, socket) do
     {:ok, assign(socket, deploy_step: "Ready!")}
   end
+
+  def handle_event("github_deploy", _value, socket) do
+    :ok = start_deploy()
+    send(self(), :create_org)
+    {:noreply, assign(socket, deploy_step: "Starting deploy...")}
+  end
+
+  def handle_info(:create_org, socket) do
+    {:ok, org} = create_org()
+    send(self(), {:create_repo, org})
+    {:noreply, assign(socket, deploy_step: "Creating GitHub org...")}
+  end
+
+  def handle_info({:create_repo, org}, socket) do
+    {:ok, repo} = create_repo(org)
+    send(self(), {:push_contents, repo})
+    {:noreply, assign(socket, deploy_step: "Creating GitHub repo...")}
+  end
+
+  def handle_info({:push_contents, repo}, socket) do
+    :ok = push_contents(repo)
+    send(self(), :done)
+    {:noreply, assign(socket, deploy_step: "Pushing to repo...")}
+  end
+
+  def handle_info(:done, socket) do
+    {:noreply, assign(socket, deploy_step: "Done!")}
+  end
+
+  defp start_deploy() do
+    sleep()
+    :ok
+  end
+
+  defp create_org() do
+    sleep()
+    {:ok, nil}
+  end
+
+  defp create_repo(_) do
+    sleep()
+    {:ok, nil}
+  end
+
+  defp push_contents(_) do
+    sleep()
+    :ok
+  end
+
+  defp sleep(), do: Process.sleep(3000)
 end
