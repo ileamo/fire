5127b46f3795a6038ac41c86041cc0cd1c079e73
imosunov <imosunov@nsg.net.ru>
2019-06-16 17:14:14 +0300

    Использовать leex

--- lib/fire_web/live/github_deploy_view.ex
+++ lib/fire_web/live/github_deploy_view.ex
@@ -2,16 +2,7 @@ defmodule FireWeb.GithubDeployView do
   use Phoenix.LiveView
 
   def render(assigns) do
-    ~L"""
-    <div class="">
-      <div>
-        <div>
-          <button phx-click="github_deploy">Deploy to GitHub</button>
-        </div>
-        Status: <%= @deploy_step %>
-      </div>
-    </div>
-    """
+    FireWeb.PageView.render("github_deploy.html", assigns)
   end
 
   def mount(_session, socket) do
